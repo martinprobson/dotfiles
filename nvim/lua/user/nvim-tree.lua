e046affa0238fbe436de171a5f3fdbd1bc61544f
Martin Robson <martin.robson@rakuten.com>
2023-08-24 16:16:58 +0100

    Update

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -3,6 +3,9 @@ require("nvim-tree").setup{
 	view = {
 		adaptive_size = true,
 		side = "left"},
+		},
+	filters = {
+		dotfiles = true},
 		}
 map('n','<leader>f',':NvimTreeToggle<CR>',{silent = true })
 map('n','<leader>n',':NvimTreeFindFile<CR>',{silent = true })
