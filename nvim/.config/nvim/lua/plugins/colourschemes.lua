da88dd2d9a38d7d54dfdda5f4892998add0bcd7a
Martin Robson <martinprobson@gmail.com>
2025-04-02 08:22:57 +0100

    Update

--- nvim/.config/nvim/lua/plugins/colourschemes.lua
+++ nvim/.config/nvim/lua/plugins/colourschemes.lua
@@ -4,17 +4,18 @@
 --                "folke/tokyonight.nvim",
 --                "xiantang/darcula-dark.nvim",
 --                "lunarvim/darkplus.nvim",
+--                "rebelot/kanagawa.nvim",
 
 return {
         {
-            "xiantang/darcula-dark.nvim",
+            "EdenEast/nightfox.nvim",
             lazy = false,
             dependencies = {
                 "nvim-treesitter/nvim-treesitter",
             },
             priority = 1000,
             config = function()
-                vim.cmd([[colorscheme darcula-dark]])
+                vim.cmd([[colorscheme nightfox ]])
             end,
         }
 }
---
+++ nvim/.config/nvim/lua/plugins/conjure.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "Olical/conjure",
+    lazy = false,
+    dependencies = {
+      "tpope/vim-repeat",
+      "guns/vim-sexp",
+      "tpope/vim-sexp-mappings-for-regular-people",
+      "tpope/vim-surround"
+    },
+  },
+}
