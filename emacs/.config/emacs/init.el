4e478bc9b7bdf013569904b792e85e7b3213fec1
Martin Robson <martin.robson@rakuten.com>
2025-04-14 09:16:05 +0100

    Update

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -88,7 +88,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-zenburn t)
+  (load-theme 'doom-gruvbox t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
